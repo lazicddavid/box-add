f4648ee69c383c0b5afff5c81b7356b3878da7eb
David Lazic <lazic.ddavid@gmail.com>
2025-11-07 23:23:34 +0100

    added .box {   display: flex;   gap: 8px;   align-items: center;   padding: 8px;   border: 1px solid black;   border-radius: 8px;   margin: 8px 6px; }

--- box-add.js
+++ box-add.js
@@ -3,18 +3,20 @@ const DOMElements = {
   addNewBoxButton: document.querySelector(".new-box-btn"),
   boxesCountSpan: document.querySelector(".paragraph1 span"),
 };
-
+//napravi funkciju createBoxList
+//metode za davanje kutija na koriscenje
 const boxList = {
   boxes: [],
 
   add() {
+    //pomeri ovu kreiranje kutija dole
     const newBox = createBox();
     this.boxes.push(newBox);
   },
 };
 
 function createBox() {
-  return { id: crypto.randomUUID() };
+  return { id: crypto.randomUUID(), marbles: 0 };
 }
 
 function updateBoxes() {
@@ -30,9 +32,8 @@ function updateBoxes() {
       <button class="delete-btn">🗑️</button>
     `;
     DOMElements.boxesContainer.appendChild(boxElement);
-
-    DOMElements.boxesCountSpan.textContent = boxList.boxes.length;
   });
+  DOMElements.boxesCountSpan.textContent = boxList.boxes.length;
 }
 
 DOMElements.addNewBoxButton.addEventListener("click", () => {
